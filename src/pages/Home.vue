f4745dd91cc483e1cf04c0a7fbffe16a5d5e5b27
362479845 <362479845@qq.com>
2018-09-27 23:27:07 +0800

    增加项目文件

---
+++ src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.text-center
+  v-card
+    v-card-text Slient is Gold ?
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    getDevices () {
+      this.$http.get('/api').then(data => {
+        console.log(data)
+      })
+    }
+  },
+  mounted () {
+  }
+}
+</script>
